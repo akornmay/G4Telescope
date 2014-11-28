a17a3127e0e6e43d904d62903a86862d13d28bb5
Andreas Kornmayer <Andreas@pb-d-128-141-25-25.cern.ch>
2014-11-28 14:29:28 +0100

    Created scrips and config file folder

---
+++ batchscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#. $G4WORKDIR/bin/Darwin-clang/pixelTB vis.mac conf/conf0.txt
+/Users/Andreas/geant4_workdir/bin/Darwin-clang/pixelTB vis.mac conf/conf0.txt &
+/Users/Andreas/geant4_workdir/bin/Darwin-clang/pixelTB vis.mac conf/conf1.txt &
+
